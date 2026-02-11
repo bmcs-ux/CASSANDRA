596067bcdc2608eba92ed8cd9718e666c46db192
bmcs-ux <56538474+bmcs-ux@users.noreply.github.com>
2026-02-11 22:55:57 +0700

    Fix and align parameter definitions for pipeline configuration

--- tests/test_parameter_config.py
+++ tests/test_parameter_config.py
@@ -7,7 +7,8 @@ class ParameterCompatibilityTests(unittest.TestCase):
     def test_legacy_aliases_exist(self):
         self.assertEqual(parameter.maxlag_test, parameter.maxlag_granger)
         self.assertEqual(parameter.alpha, parameter.alpha_granger)
-        self.assertEqual(parameter.lookback_days, parameter.fred_lookback_days)
+        self.assertIsInstance(parameter.lookback_days, int)
+        self.assertEqual(parameter.base_interval, "1d")
 
 
 if __name__ == '__main__':
